39e09ec527fee0b7ce5dc05f57d8a6e0ceb1f711
Cédric Pahud <cedric.pahud@he-arc.ch>
2017-11-29 18:11:35 +0100

    régler le problème du gender et du password en claire

--- DrunkYet/app/Http/Controllers/RegistrationController.php
+++ DrunkYet/app/Http/Controllers/RegistrationController.php
@@ -16,12 +16,19 @@ class RegistrationController extends Controller
 
             'name'  =>  'required|min:2|string',
             'email'  =>  'required|email|unique:users',
-            'password'  =>  'required|min:6',
+            'password'  =>  'required|min:6|confirmed',
             'weight' => 'required|integer|min:10',
             'birth' => 'required|date|before:today'
         ]);
 
-        $user = User::create(request(['name', 'email', 'birth', 'password', 'birth']));
+        $user = User::create([
+            'name' => request('name'),
+            'email' => request('email'),
+            'password' => bcrypt(request('password')),
+            'weight' => request('weight'),
+            'birth' => request('birth'),
+            'gender' => request('gender')
+        ]);
         auth()->login($user);
 
         return redirect('/user');
--- DrunkYet/resources/views/registration/create.blade.php
+++ DrunkYet/resources/views/registration/create.blade.php
@@ -6,26 +6,29 @@
     {{ csrf_field() }}
 
     <label for="name">Name :</label>
-    <input type="text" id="name" name="name" value="">
+    <input type="text" id="name" name="name" value="" required>
 
     <div class="dy-radio-group">
-        <input checked id="male" type=radio name="gender"/>
+        <input checked id="male" value="male" type=radio name="gender" checked/>
         <label for="male">Homme</label>
-        <input id="female" type=radio name="gender"/>
+        <input id="female" value="female" type=radio name="gender"/>
         <label for=female>Femme</label>
     </div>
 
     <label for=birth"">Birth date :</label>
-    <input type="date" id="birth" name="birth"/></br>
+    <input type="date" id="birth" name="birth" required/></br>
 
     <label for=weight"">Weight :</label>
-    <input type="number" id="weight" name="weight"/>
+    <input type="number" id="weight" name="weight" required/>
 
     <label for=email"">Email :</label>
-    <input type="email" id="email" name="email" value="">
+    <input type="email" id="email" name="email" value="" required/>
 
     <label for="password">Password :</label>
-    <input type="password" id="password" name="password" value="">
+    <input type="password" id="password" name="password" value="" required/>
+
+    <label for="password_confirmation">Confirm :</label>
+    <input type="password" id="password_confirmation" name="password_confirmation" value=""required/>
 
     <button type="submit" name="button" class="dy-button-strong">Register</button>
 </form>
