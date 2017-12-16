4a241b32fc1cb6818d8d13a38e768a67bdcaf552
paul.jeanbour <pa.jeanbourquin@gmail.com>
2017-12-16 17:45:15 +0100

    Work on #37
    Try a thing :dog:

--- DrunkYet/app/Providers/AppServiceProvider.php
+++ DrunkYet/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if(env('APP_ENV') == 'production'){
+            \URL::forceScheme('https');
+        }
+        else{
+            \URL::forceScheme('http');
+        }
     }
 
     /**
