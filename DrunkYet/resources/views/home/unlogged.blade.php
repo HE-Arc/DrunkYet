553ccb433af45512b2ec4dda015f90d9d829060c
paul.jeanbour <pa.jeanbourquin@gmail.com>
2017-12-16 17:33:51 +0100

    Work on #37
    Try something which seems good.

--- DrunkYet/resources/views/home/unlogged.blade.php
+++ DrunkYet/resources/views/home/unlogged.blade.php
@@ -7,7 +7,7 @@
 <img src="{{ URL::asset('/home_1024.png')}}">
 @endif
 <div class="dy-space-1"></div>
-<a href="/login"><div class="dy-button-strong">Se Connecter</div></a>
+<a href="{{URL::action('SessionsController@create')}}"><div class="dy-button-strong">Se Connecter</div></a>
 <a href="/register"><div class="dy-button-strong">Créer un compte</div></a>
 <a href="/guest"><div class="dy-button-normal">Continuer comme invité</div></a>
 @endsection
